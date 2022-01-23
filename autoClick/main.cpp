b1674eed378cf0e851b4d384c5d11dcd68ede75c
stack500 <89987877+stack500@users.noreply.github.com>
2022-01-24 00:46:19 +1100

    Update main.cpp

--- autoClick/main.cpp
+++ autoClick/main.cpp
@@ -12,11 +12,11 @@ int main()
     std::cout << "F5. Set Delay (milliseconds)\n" << std::endl;
     std::cout << "F8. Exit" << std::endl;
 
-    Clicker clicker(50);
+    Clicker clicker(50);                                    //create clicker object, parse 50ms argument to our constructor to initialize our cDelay property.
 
     while (!GetAsyncKeyState(VK_F8) & 0x01) {               //Main Loop
 
-                                              //create clicker object, parse 50ms argument to our constructor to initialize our cDelay property.
+                                             
 
         if (GetAsyncKeyState(VK_F3) & 0x01) {               //Start
             clicker.startClick();
